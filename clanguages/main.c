d4e84257ea61771fda404b3a382008824e9fd060
jecky <jecky@sing.com>
2022-01-11 09:27:15 +0700

    goto

--- clanguages/main.c
+++ clanguages/main.c
@@ -3,159 +3,218 @@
 #include <stdbool.h>
 
 int main() {
-  // 3-8 条件分支语句
-
-  // Bool 类型
-  printf("============== Bool 类型 ===========");
+  // 3-9 循环语句
+  printf("=================== 1、循环 while ============= \n");
   /**
-   * Bool 类型
-   * true: 1, false: 0
-   * _Bool, bool, // 在 C900 里是看不到的，只有新版里才有
+   * while 是一开始就看条件
+   * while (<condition>) { // 只要 condition 成立，里的程序一直会执行
    *
-   * _Bool 就是一个宏，它本质就是 1
-   */
-
-  _Bool is_enabled = true; // true 就是 1
-  is_enabled = 10;
-  printf("is_enabled 的结果是：%d\n", is_enabled);
-
-  /*
-    is_enabled 给它赋值了 10，打印结果它还是1，它就这么个作用
-    其实它通过编译的手段，强制让这 is_enabled 的值只能是 1 或者是 0，
-    我们用的过程呢：
-    就只能 is_enabled = false;
-    源码宏 true 就是1, false 就是 0
-    #define bool _Bool
-    #define true 1
-    #define false 0
-   */
-
-  is_enabled = false;
-
-  /**
-   * C 语言它学得它这个 _Bool 这个名字，类型下划_有点丑，或者用
-   * 起来不方便，所以它又定义了一个 宏 bool，所以我们平时写代码的
-   * 时候也可以这么写
-   * bool flag = true;
-   *
-   */
-  // 以上的 _Bool 的写也可以写成如下
-  bool is_visible = false;
-
-
-  // if 语句
-  printf("============== if 语句  ===========");
-  /**
-   * // 一个条件判断
-   * if(<condition>){ // 如果 condition 这个值为 true 就执行下面的
-   *    ...statement
-   * } else { // 否则就执行 else 这下面的
-   *    ...statement
-   * }
-   *
-   * // 多个条件判断
-   * if(<condition1>){ // 如果 condition 这个值为 true 就执行下面的
-   *    ...statement
-   * } else if(<condition2>){ // 否则就执行 else 这下面的
-   *    ...
-   * }else { // 否则就执行 else 这下面的
-   *    ...statement
    * }
    */
 
-  // 猜数字的游戏
-  /*
- #define MAGIC_NUMBER 10 // 定义一个“宏”的数字
-  int user_input;
-  printf("Please input a number: \n");
-  scanf("%d", &user_input); // &user_input
-  if (user_input > MAGIC_NUMBER) {
-    printf("Your number is bigger!");
-  } else if (user_input < MAGIC_NUMBER) {
-    printf("Your number is smaller!");
-  } else {
-    printf("Yes ！You got it!");
-  }
-  */
+  // 例子，以下是定义宏
+/*#define ADD '+'
+#define SUB '-'
+#define MULTIPLY '*'
+#define DIVIDE '/'
+#define REM '%'
 
-  // 不推荐以下的写法，容易报错 use bracket!
-  /*
-   if (is_enabled) {
-    if (is_visible) printf("is_visible");
-    else printf("is_invisible");
-   }
-   */
+  int left;
+  int right;
+
+  char operator;
+
+  char command = 'y';
+
+  while (command != 'q') {
+
+    printf("Please input an expression: \n");
+    scanf("%d %c %d", &left, &operator, &right);
+
+    int result;
+    switch (operator) {
+      case ADD:result = left + right;
+        break;
+      case SUB:result = left - right;
+        break;
+      case MULTIPLY:result = left * right;
+        break;
+      case DIVIDE:result = left / right;
+        break;
+      case REM:result = left % right;
+        break;
+      default:printf("Unsupported operation: %c\n", operator);
+        return 1;
+    }
+
+    printf("result 的结果是：%d\n", result);
+
+    printf("Again? Type 'q' for quit: \n ");
+    // 也可以使用 puts("Again? Type 'q' for quit:"); // 相当于不加 \n 换行了
+
+    // scanf("%c", &command); // 用 scanf 还要格式化
+    getchar(); // 先获取主要是解决缓存方面的问题
+    command = getchar(); //  代替 scanf("%c", &command); 的写法
+
+    printf("%d\n", command);
+  }*/
 
 
-  // ?: 三元运算符
-  printf("============== ?: 三元运算符  ===========\n");
+  printf("=================== 2、循环 do while ============= \n");
   /**
-   * ?: -> <expr> ? <expr1> : <expr2>
-   * 为 true  返回 expr == true, expr1
-   * 为 false 返回 expr == false, expr2
-   */
-  int is_open = is_enabled && is_visible ? 1 : 0;
-  printf("is_open %d\n", is_open); // is_open 0
-
-
-  // switch 语句
-  printf("============== switch 语句 ===========\n");
-  /**
-   * switch 主要是用来是对于整数的判断
-   * switch (<cond>) {
-   *    case 0: {
-   *        ....
-   *    }
-   *    break;
-   *    case 1: {
-   *        ....
-   *    }
+   * do while 是先开干活，再看条件
+   * do{
+   *    ...执行
+   * } while (条件);
    *
-   *    default: {
-   *
-   *    }
-   *
-   * }
-   *
+   * 用 do while 来改造
    */
 
-// 例子，以下是定义宏
+/*
 #define ADD '+'
 #define SUB '-'
 #define MULTIPLY '*'
 #define DIVIDE '/'
 #define REM '%'
 
-    int left;
-    int right;
+  int left;
+  int right;
 
-    char operator;
-  printf("Please input an expression: \n");
-  scanf("%d %c %d", &left, &operator, &right);
+  char operator;
 
-  int result;
-  switch(operator){
-    case ADD:
-      result = left + right;
-      break;
-    case SUB:
-      result = left - right;
-      break;
-    case MULTIPLY:
-      result = left * right;
-      break;
-    case DIVIDE:
-      result = left / right;
-      break;
-    case REM:
-      result = left % right;
-      break;
-    default:
-      printf("Unsupported operation: %c\n", operator);
-      return 1;
+  char command = 'y';
+
+  do {
+
+    printf("Please input an expression: \n");
+    scanf("%d %c %d", &left, &operator, &right);
+
+    int result;
+    switch (operator) {
+      case ADD:result = left + right;
+        break;
+      case SUB:result = left - right;
+        break;
+      case MULTIPLY:result = left * right;
+        break;
+      case DIVIDE:result = left / right;
+        break;
+      case REM:result = left % right;
+        break;
+      default:printf("Unsupported operation: %c\n", operator);
+        return 1;
+    }
+
+    printf("result 的结果是：%d\n", result);
+
+    printf("Again? Type 'q' for quit: \n ");
+    // 也可以使用 puts("Again? Type 'q' for quit:"); // 相当于不加 \n 换行了
+
+    // scanf("%c", &command); // 用 scanf 还要格式化
+    getchar(); // 先获取主要是解决缓存方面的问题
+    command = getchar(); //  代替 scanf("%c", &command); 的写法
+
+    printf("%d\n", command);
+  } while (command != 'q');*/
+
+  printf("=================== 3、循环 for ============= \n ");
+  /**
+   *  for 循环
+   *
+   *  for(<initialization>; <condition> ; < state){
+   *    ...statement
+   *  }
+   *
+   *  < c99 之前，
+   *   initialization 不能有变量定义
+   */
+
+  /*
+  int num = 1;
+  int sum = 0;
+  while(num < 100) {
+    sum += num;
+    num++;
   }
 
-  printf("result 的结果是：%d\n", result);
+  printf("sum of [1, 100]: %d\n", sum); // sum of [1, 100]: 4950
+  */
+
+
+  // 用 for 循环来改造
+  /*
+   * C99 之前
+   */
+  /*
+  int num = 1;
+  int sum = 0;
+  for(num = 0; num < 100; ++num){
+    sum += num;
+  }
+  printf("sum of [1, 100]: %d\n", sum); // sum of [1, 100]: 4950
+  */
+
+
+  // 用 for 循环来改造
+  /*
+   * C99 开始
+   */
+  int sum = 0;
+  for(int num = 0; num < 100; ++num){
+    sum += num;
+  }
+  printf("sum of [1, 100]: %d\n", sum); // sum of [1, 100]: 4950
+
+  printf("===== 两个循环\n ====");
+  int sumij = 0;
+  for(int i = 0, j = 0; i <= 100 && j <= 100; ++i, ++j){
+    sumij += i * (i + j);
+  }
+  printf("sumij: %d\n", sumij); //  ====sumij: 676700
+
+  // continue break;
+  /**
+   * continue   是执行下一次循环
+   * break      是跳过循环
+   */
+  printf("===== continue break;\n ====");
+  for(int i = 0; i < 10; ++i){
+    if(i == 2) continue;
+    if(i == 8) break;
+    printf("i: %d\n", i);
+  }
+  /*
+    结果如下：
+    i: 1
+    i: 3
+    i: 4
+    i: 5
+    i: 6
+    i: 7
+    */
+
+
+  // goto 任意跳转, 可以跳转到任意的标签
+  printf("======= 4、goto 任意跳转, 可以跳转到任意的标签======= \n");
+  /**
+   * goto 任意跳转, 可以跳转到任意的标签
+   */
+   int i = 0;
+   // 定义标签 begin:，这个也是一个标识符
+   begin:
+  printf("%d\n", i);
+  if(i++ < 5) { // goto 外面一般都有一个 if 语句，不然就死循环了
+    goto begin;
+  }
+  /*  在项目当中，一定不要使用 goto 用 while 循环就可以了
+    0
+    1
+    2
+    3
+    4
+    5
+   */
+
 
 
 
